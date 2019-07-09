d970b6545814fa1734d91c31e5f2f7d0200c2387
Christopher Coram <ultraartist15@gmail.com>
2019-07-09 15:27:28 -0400

    Update EnemyShipLayer3.py

--- Level One/EnemyShipLayer3.py
+++ Level One/EnemyShipLayer3.py
@@ -12,23 +12,26 @@ pygame.init()
 size = [800, 600]
 screen = pygame.display.set_mode(size)
 
+# Background variables
+background_image = pygame.image.load("SPoW_82318_01 copy.png")
+
 # Total enemy ships
 # Ship one
-EnemyShipOne = pygame.image.load("caa0a03c64dbc3d2fa10e7056afb006f.png").convert()
+EnemyShipOne = pygame.image.load("alien1.png").convert()
 EnemyShipOne = pygame.transform.scale(EnemyShipOne, (80,80))
 # Ship Two
-EnemyShipTwo = pygame.image.load("caa0a03c64dbc3d2fa10e7056afb006f.png").convert()
+EnemyShipTwo = pygame.image.load("alien1.png").convert()
 EnemyShipTwo = pygame.transform.scale(EnemyShipTwo, (80,80))
 # Ship Three
-EnemyShipThree = pygame.image.load("caa0a03c64dbc3d2fa10e7056afb006f.png").convert()
+EnemyShipThree = pygame.image.load("alien1.png").convert()
 EnemyShipThree = pygame.transform.scale(EnemyShipThree, (80,80))
 ###################
 
-# -------- CLASSES -------- 
+# -------- CLASSES --------
 class LayerThree():
     def __init__(self):
         print("Testing when called upon")
-        
+
     def EnemyLayer(self):
         # Spaceship One
         screen.blit(EnemyShipOne, (280,100)) # updates screen with enemy ship
@@ -36,3 +39,5 @@ class LayerThree():
         screen.blit(EnemyShipTwo, (360,100)) # updates screen with enemy ship
         # Spaceship Three
         screen.blit(EnemyShipThree, (440,100)) # updates screen with enemy ship
+
+screen.blit(background_image, (0,0))

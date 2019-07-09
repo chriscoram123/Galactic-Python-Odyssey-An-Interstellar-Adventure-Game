165f18f9472d934a78194615efa12ba28df84251
Christopher Coram <ultraartist15@gmail.com>
2019-07-09 15:28:40 -0400

    Update EnemyShipsLayer2.py

--- Level One/EnemyShipsLayer2.py
+++ Level One/EnemyShipsLayer2.py
@@ -12,32 +12,35 @@ pygame.init()
 size = [800, 600]
 screen = pygame.display.set_mode(size)
 
+# Background variable
+background_image = pygame.image.load("SPoW_82318_01 copy.png")
+
 # Total enemy ships
 # Ship one
-EnemyShipOne = pygame.image.load("Spaceship-Transparent-PNG.png").convert()
+EnemyShipOne = pygame.image.load("Spaceship-Transparent-PNG copy.png").convert()
 EnemyShipOne = pygame.transform.scale(EnemyShipOne, (80,80))
 # Ship Two
-EnemyShipTwo = pygame.image.load("Spaceship-Transparent-PNG.png").convert()
+EnemyShipTwo = pygame.image.load("Spaceship-Transparent-PNG copy.png").convert()
 EnemyShipTwo = pygame.transform.scale(EnemyShipTwo, (80,80))
 # Ship Three
-EnemyShipThree = pygame.image.load("Spaceship-Transparent-PNG.png").convert()
+EnemyShipThree = pygame.image.load("Spaceship-Transparent-PNG copy.png").convert()
 EnemyShipThree = pygame.transform.scale(EnemyShipThree, (80,80))
 # Ship Four
-EnemyShipFour = pygame.image.load("Spaceship-Transparent-PNG.png").convert()
+EnemyShipFour = pygame.image.load("Spaceship-Transparent-PNG copy.png").convert()
 EnemyShipFour = pygame.transform.scale(EnemyShipFour, (80,80))
 # Ship Five
-EnemyShipFive = pygame.image.load("Spaceship-Transparent-PNG.png").convert()
+EnemyShipFive = pygame.image.load("Spaceship-Transparent-PNG copy.png").convert()
 EnemyShipFive = pygame.transform.scale(EnemyShipFive, (80,80))
 # Ship Six
-EnemyShipSix = pygame.image.load("Spaceship-Transparent-PNG.png").convert()
+EnemyShipSix = pygame.image.load("Spaceship-Transparent-PNG copy.png").convert()
 EnemyShipSix = pygame.transform.scale(EnemyShipSix, (80,80))
 ###################
 
-# -------- CLASSES -------- 
+# -------- CLASSES --------
 class LayerTwo():
     def __init__(self):
         print("Testing when called upon")
-        
+
     def EnemyLayer(self):
         # Spaceship One
         screen.blit(EnemyShipOne, (40,100)) # updates screen with enemy ship
@@ -51,3 +54,6 @@ class LayerTwo():
         screen.blit(EnemyShipFive, (600,100)) # updates screen with enemy ship
         # Spaceship Six
         screen.blit(EnemyShipSix, (680,100)) # updates screen with enemy ship
+
+
+screen.blit(background_image, (0,0))
